a576baac8b362bcce7594ed23cc30a42bf629ed7
FranciscoZanonDP <franciscozanon2002@gmail.com>
2025-07-31 18:17:16 -0300

    botcito test

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const supabaseUrl = process.env['SUPABASE_URL'];
-const supabaseServiceKey = process.env['SUPABASE_SERVICE_KEY'];
+const supabaseServiceKey = process.env['SUPABASE_SERVICE_ROLE_KEY'];
 
 if (!supabaseUrl || !supabaseServiceKey) {
-  throw new Error('SUPABASE_URL y SUPABASE_SERVICE_KEY son requeridos en las variables de entorno');
+  throw new Error('SUPABASE_URL y SUPABASE_SERVICE_ROLE_KEY son requeridos en las variables de entorno');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseServiceKey, {
